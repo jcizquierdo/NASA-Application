b865d21570d3d4ff3be17b078eb82ca28a0a66b5
Russell Morgan <rhmorgan13@comcast.net>
2020-05-01 14:48:30 -0500

    able to enter date and list

--- assets/js/nasa.js
+++ assets/js/nasa.js
@@ -2,20 +2,37 @@ let main = document.getElementById('main');
 
 
 function fetchNearEarthObjects() {
-      fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-01-01&end_date=2021-01-01&api_key=58quQOPaEHya8ShD5JVzTSjU2Ece7FNGSAFe9rVT')
+  let date = document.getElementById('searchDate').value;
+  console.log("date="+date);
+      fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date='+date+'&end_date='+date+
+        '&api_key=58quQOPaEHya8ShD5JVzTSjU2Ece7FNGSAFe9rVT')
       .then((response) => {
         return response.json();
       })
       .then((response) => {
         console.log(response);
-        let name = document.getElementById('name');
-        name.textContent = response.near_earth_objects["2021-01-01"][0].name;
 
-        let jplUrl = document.getElementById('jplLink')
-        jplUrl.href = response.near_earth_objects["2021-01-01"][0].nasa_jpl_url;
+        let neoLength = response.near_earth_objects[date].length;
+        console.log("neo = "+neoLength);
+        for (let x = 0; x < neoLength; x++) {
+          let name = document.createElement('p');
+          name.textContent = response.near_earth_objects[date][x].name;
+          main.appendChild(name);
+
+          let jplUrl = document.createElement('a');
+          jplUrl.textContent = "JPL Link";
+          jplUrl.href = response.near_earth_objects[date][x].nasa_jpl_url;
+          main.appendChild(jplUrl);
+        }
+
+        //let name = document.getElementById('name');
+        //name.textContent = response.near_earth_objects[date][0].name;
+
+        //let jplUrl = document.getElementById('jplLink')
+        //jplUrl.href = response.near_earth_objects[date][0].nasa_jpl_url;
         
       });
 }
 
-
-fetchNearEarthObjects();
+// near_earth_objects["2010-05-01"]
+//fetchNearEarthObjects();

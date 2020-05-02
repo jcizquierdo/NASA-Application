95623ad61f6041e6cecb34dee4aadbbb1a659a1b
Conrad Trost <ctrost@tamu.edu>
2020-05-01 21:20:54 -0500

    Used Foundation to create card for json response + updated js file with jQuery

--- assets/js/nasa.js
+++ assets/js/nasa.js
@@ -13,27 +13,30 @@ function fetchNearEarthObjects() {
       .then((response) => {
         console.log(response);
 
+        // create indiv containers for each data object
+
         let neoLength = response.near_earth_objects[date].length;
         console.log("neo = "+neoLength);
+
         for (let x = 0; x < neoLength; x++) {
-          let name = document.createElement('p');
-          name.textContent = response.near_earth_objects[date][x].name;
-          main.appendChild(name);
+          // Create Card Div 
+          $('#main').append('<div id='+x+' class="card"></div>');
+          $('this').addClass('info-contain');
 
-          let jplUrl = document.createElement('a');
-          jplUrl.textContent = "JPL Link";
-          jplUrl.href = response.near_earth_objects[date][x].nasa_jpl_url;
-          main.appendChild(jplUrl);
+          // Name of Object
+          var name = response.near_earth_objects[date][x].name;
+          $('#'+x).append('<div class="card-divider">'+name+'</div>');
+
+          // Diameter 
+          let diamRef = response.near_earth_objects[date][x].estimated_diameter.meters
+          minRef = diamRef.estimated_diameter_min.toFixed(2);
+          maxRef = diamRef.estimated_diameter_max.toFixed(2);
+          $('#'+x).append('<div class="card-section">Diameter between '+minRef+' and '+maxRef+' meters.</div>')
+
+          // JPL URL
+          var url = response.near_earth_objects[date][x].nasa_jpl_url;
+          $('#'+x).append('<div class="card-section"><a href="'+url+'">JPL Link</a></div>')
         }
-
-        //let name = document.getElementById('name');
-        //name.textContent = response.near_earth_objects[date][0].name;
-
-        //let jplUrl = document.getElementById('jplLink')
-        //jplUrl.href = response.near_earth_objects[date][0].nasa_jpl_url;
         
       });
 }
-
-// near_earth_objects["2010-05-01"]
-//fetchNearEarthObjects();

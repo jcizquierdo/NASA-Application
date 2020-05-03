726cbc3acdca64e35cedcbd3ccf7e50a28c5ec9c
JC Izquierdo <josecarlosizquierdo1994@gmail.com>
2020-05-02 21:18:13 -0500

    added some front end details

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$("#searchDate").datepicker({
+    dateFormat: "yy-mm-dd",
+    orientation: "bottom",
+    changeMonth: true,
+    changeYear: true,
+    yearRange: "-100:+0",
+    
+});
+
+});
